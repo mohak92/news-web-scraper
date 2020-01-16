4ddf309a67e7f888f2b6c2ab372846b30b703b90
mohak92 <tamhane.mohak@gmail.com>
2020-01-15 23:10:29 -0500

    UI fixes timeout added

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,27 +1,30 @@
-$(document).ready(function() {
-    $(document).on("click", ".scrape-new", handleArticleScrape);
-    $(document).on("click", ".clear", handleArticleClear);
+$(document).ready(function () {
+  $(document).on("click", ".scrape-new", scrapeArticle);
+  $(document).on("click", ".clear", clearArticle);
+  $(document).on("click", ".save", saveArticle);
 
-    function handleArticleScrape() {
-        // This function handles the user clicking any "scrape new article" buttons
-        $.get("/api/fetch").then(function(data) {
-          // If we are able to successfully scrape the NYTIMES and compare the articles to those
-          // already in our collection, re render the articles on the page
-          // and let the user know how many unique articles we were able to save
-          // initPage();
-          console.log(data)
-          // data.message = "Scrape completed!"
-          // bootbox.alert($("<h3 class='text-center m-top-80'>").text(data.message));
+  function scrapeArticle() {
+    // This function handles the user clicking any "scrape new article" buttons
+    $.get("/api/fetch").then(function (data) {
+      console.log(data)
+      setTimeout(
+        function () {
           window.location.href = "/";
-        });
-      }
+        }, 2000);
+    });
+  }
 
-      function handleArticleClear() {
-        $.get("api/clear").then(function(data) {
-          console.log(data)
-          articleContainer.empty();
-          // initPage();
-          location.reload();
-        });
-      }
+  //This function sends request to server to delete article in the collection
+  function clearArticle() {
+    $.get("/api/clear").then(function (data) {
+      console.log(data)
+      $(".articleContainer").empty();
+      location.reload();
+    });
+  }
+
+  //This function is called when user click save article, send a request to server to update the document in collection to be saved.
+  function saveArticle() {
+
+  }
 });
